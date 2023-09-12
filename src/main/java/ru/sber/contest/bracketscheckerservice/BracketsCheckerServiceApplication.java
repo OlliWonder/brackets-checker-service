979d2544b9ab9b9b8784eb31289306b822d04127
Olga Ivlieva <sunchess55@gmail.com>
2023-09-12 10:24:24 +0300

    first commit

---
+++ src/main/java/ru/sber/contest/bracketscheckerservice/BracketsCheckerServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.sber.contest.bracketscheckerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BracketsCheckerServiceApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(BracketsCheckerServiceApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/ru/sber/contest/bracketscheckerservice/BracketsCheckerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sber.contest.bracketscheckerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BracketsCheckerServiceApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
