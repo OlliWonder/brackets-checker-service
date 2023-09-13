5858fcfc37f28fc745855fc8e04cbc559a619755
Olga Ivlieva <sunchess55@gmail.com>
2023-09-13 23:43:56 +0300

    almost done (service test is in work yet)

--- src/test/java/ru/sber/contest/bracketscheckerservice/service/TextServiceTest.java
+++ src/test/java/ru/sber/contest/bracketscheckerservice/service/TextServiceTest.java
@@ -1,13 +1,4 @@
 package ru.sber.contest.bracketscheckerservice.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import ru.sber.contest.bracketscheckerservice.api.request.TextCheckRequest;
-import ru.sber.contest.bracketscheckerservice.api.response.TextCheckResponse;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class TextServiceTest {
 }
